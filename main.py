c73f5c3de8881131a7dd074bde307d66f65c80fa
Alex Tran <alextran1357@gmail.com>
2025-01-07 23:46:23 -0800

    update prompt

--- main.py
+++ main.py
@@ -7,24 +7,25 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI()
 
-audio_file = open("sound_recordings/recording_test.mp3", "rb")
+audio_file = open("sound_recordings/Recording_low_quality.mp3", "rb")
 transcript = client.audio.transcriptions.create(
   prompt="You are a doctor speaking",
   model="whisper-1",
   file=audio_file
 )
+print("-------------------------------------------")
 print("Text ", transcript.text)
-print("Transcription ", transcript)
+# print("Transcription ", transcript)
 
 completion = client.chat.completions.create(
     model="gpt-4o",
     messages=[
-        {"role": "developer", "content": "Summarize the provided text"},
+        {"role": "developer", "content": "Summarize the provided text. Make it more concise and shorter. Provide text as if taking down quick notes"},
         {
             "role": "user",
             "content": transcript.text
         }
     ]
 )
-
-print(completion.choices[0].message)
+print("-------------------------------------------")
+print(completion.choices[0].message.content)
